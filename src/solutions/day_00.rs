ed1c32a21a62f74e0800c34e4e9bf68851c3683b
Ben-Wormald <benwormald95@gmail.com>
2023-11-29 11:50:07 +0000

    add tests template

--- src/solutions/day_00.rs
+++ src/solutions/day_00.rs
@@ -5,3 +5,24 @@ pub fn solve_part_one(input: &str) -> String {
 pub fn solve_part_two(input: &str) -> String {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part_one() {
+        let expected = "";
+
+        assert_eq!(solve_part_one(INPUT), expected);
+    }
+
+    #[test]
+    fn part_two() {
+        let expected = "";
+
+        assert_eq!(solve_part_two(INPUT), expected);
+    }
+}
